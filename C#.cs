6a6a509b67360b3e74a163139bdf83f89b3544cf
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-19 01:33:35 +0600

    Create C#.cs

---
+++ C#.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+    double a,b,c,v;
+    int n,i;
+    n=Convert.ToInt32(Console.ReadLine());
+    for(i=0;i<n;i++)
+    {
+        var p = Console.ReadLine().Split();
+        a = Double.Parse(p[0]);
+        b = Double.Parse(p[1]);
+        c = Double.Parse(p[2]);
+        v=(a*2+b*3+c*5)/10.0;
+     Console.WriteLine(string.Format("{0:0.0}", v));
+    }
+    }
+}
